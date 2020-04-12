de84895873c7add166cdcacce5ecd8ef07eed900
asif immanad <asifimmanad.tech@gmail.com>
2020-04-12 16:50:28 +0000

    D2:better Loop to print table of 5

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,13 +1,14 @@
 public class HelloGit {
 
-    void myMethod(){
+    static void myMethod(){
+        for(int i = 1; i <= 10; i++){
             System.out.println(i*5);
         }
     }
 
     public static void main(String[] args) {
 
-
+        myMethod();
 
     }
 }
