786908c29313e3b43468d0328b12460e91308750
asif708 <asif_immanad@persistent.co.in>
2020-04-12 22:24:36 +0530

    d2: table generated by value n

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,13 +1,17 @@
 public class HelloGit {
 
-    void myMethod(){
-            System.out.println(i*5);
+    static void myMethod(int n){
+        
+        for(int i = 1; i <=10; i++){
+                System.out.println(i*n);
         }
+        
+        
     }
 
     public static void main(String[] args) {
 
-
+        myMethod(12);
 
     }
 }
